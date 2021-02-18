cf69bb6629571fada5eba13e2e1d0a796294ad0a
Nikita <justnicke@gmail.com>
2021-02-18 17:51:01 +0300

    working request  testFunc + testDict

--- F1 Space/Modules/HistoricalModules/Detail/Views/HistoricalDriverStandingsViewController.swift
+++ F1 Space/Modules/HistoricalModules/Detail/Views/HistoricalDriverStandingsViewController.swift
@@ -19,8 +19,10 @@ final class HistoricalDriverStandingsViewController: BaseHistoricalDetailViewCon
         fatalError("init(coder:) has not been implemented")
     }
     
-    var arrays = [[ResultF1]]()
+    
     var items: [String : [[String : Int]]] = [:]
+    
+    var res = [[ResultF1]]()
     
     let duelView = DuelView()
     
@@ -28,128 +30,102 @@ final class HistoricalDriverStandingsViewController: BaseHistoricalDetailViewCon
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        
         view.addSubview(duelView)
         duelView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor,
                         padding: .init(top: 200, left: 20, bottom: 0, right: 20),
                         size: .init(width: 0, height: 120))
         
-        print(driver)
-        
-        DispatchQueue.main.async {
-            self.testFunc()
-        }
-        
-        DispatchQueue.main.async {
-            self.test2()
-        }
-        
-        
+
+        testFunc()
     }
     
-    
-    func test2() {
-//            testFunc()
- 
-            print(self.arrays.map{$0.map{$0.driver.driverID}})
+    let semaphore = DispatchSemaphore(value: 0)
+
+    func testFunc() {
+        guard let driver = self.viewModel?.driverID else { return }
+        guard let constID = self.viewModel?.constructorsID else { return }
+        var counter = 0
         
-    }
-    
-    func testFunc(){
-        
-        guard let driver = viewModel?.driverID else { return }
-        guard let constID = viewModel?.constructorsID else { return }
-        
-        for id in constID {
-            API.requestConstructorDetailResult(year: "2020", id: id) { [weak self] (const, err) in
-                guard let results = const?.constructorDetailData.constructorDetailTable.races.map({ $0.results }) else { return }
-                
-                for res in results {
+        DispatchQueue.global(qos: .utility).async {
+            for id in constID {
+                API.requestConstructorDetailResult(year: "2020", id: id) { [weak self] (const, err) in
                     
-                    // statistics of current season
-                    for i in res {
-                        if i.driver.driverID.contains(driver) {
-                            // position
-                            if i.position == "1" {
-                                self?.winCount += 1
-                                self?.podiumCount += 1
-                            } else if i.position == "2" || i.position == "3" {
-                                self?.podiumCount += 1
-                            }
-                            
-                            // pole or grid
-                            if i.grid == "1" {
-                                self?.poleCount += 1
-                            }
-                            
-                            // bestGrid
-                            if let bestG = Int(i.grid) {
-                                if self?.bestGrid == 0 {
-                                    self?.bestGrid = bestG
-                                } else if bestG != 0 && self!.bestGrid > bestG {
-                                    self?.bestGrid = bestG
+                    guard let results = const?.constructorDetailData.constructorDetailTable.races.map({ $0.results }) else { return }
+                    
+                    for (_, res) in results.enumerated() {
+                        // statistics of current season
+                        for i in res {
+                            if i.driver.driverID.contains(driver) {
+                                // position
+                                if i.position == "1" {
+                                    self?.winCount += 1
+                                    self?.podiumCount += 1
+                                } else if i.position == "2" || i.position == "3" {
+                                    self?.podiumCount += 1
+                                }
+                                
+                                // pole or grid
+                                if i.grid == "1" {
+                                    self?.poleCount += 1
+                                }
+                                
+                                // bestGrid
+                                if let bestG = Int(i.grid) {
+                                    if self?.bestGrid == 0 {
+                                        self?.bestGrid = bestG
+                                    } else if bestG != 0 && self!.bestGrid > bestG {
+                                        self?.bestGrid = bestG
+                                    }
+                                }
+                                
+                                // bestFinish
+                                if let bestF = Int(i.position) {
+                                    if self?.bestFinish == 0 {
+                                        self?.bestFinish = bestF
+                                    } else if self!.bestFinish > bestF {
+                                        self?.bestFinish = bestF
+                                    }
+                                }
+                                
+                                // retire
+                                if i.positionText == "R" {
+                                    self?.numOfRetire += 1
+                                }
+                                
+                                // permanentNumber
+                                self?.permanentNumber = i.driver.permanentNumber
+                                
+                                // nationality
+                                self?.nationality = i.driver.nationality
+                                
+                                // fastestLap
+                                if i.fastestLap?.rank == "1" {
+                                    self?.fastestLapCount += 1
                                 }
                             }
-                            
-                            // bestFinish
-                            if let bestF = Int(i.position) {
-                                if self?.bestFinish == 0 {
-                                    self?.bestFinish = bestF
-                                } else if self!.bestFinish > bestF {
-                                    self?.bestFinish = bestF
-                                }
-                            }
-                            
-                            // retire
-                            if i.positionText == "R" {
-                                self?.numOfRetire += 1
-                            }
-                            
-                            
-                            // permanentNumber
-                            self?.permanentNumber = i.driver.permanentNumber
-                            
-                            // nationality
-                            self?.nationality = i.driver.nationality
-                            
-                            // fastestLap
-                            if i.fastestLap?.rank == "1" {
-                                self?.fastestLapCount += 1
-                            }
-                            
-                            
+                        }
+                        
+                        // Get data with selected driver and teammate
+                        let trueOrFalse = res.compactMap ({ $0.driver.driverID.contains(driver) })
+                        
+                        if trueOrFalse.contains(true) {
+                            self?.numOfRace += 1
+                            self?.res.append(res)
                         }
                     }
                     
-                    // Get data with selected driver and teammate
-                    let trueOrFalse = res.compactMap ({ $0.driver.driverID.contains(driver) })
+                    counter += 1
                     
-                    if trueOrFalse.contains(true) {
-                        self?.numOfRace += 1
-                        self?.arrays.append(res)
+                    if counter == constID.count  {
+                        self?.semaphore.signal()
                     }
                 }
             }
             
+            self.semaphore.wait()
+            self.testDict(res: self.res)
+            print(self.items)
         }
-        
-        
-//        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
-//            print(self.arrays.map{$0.map{$0.driver.driverID}})
-////            print(self.numOfRace)
-////            print(self.winCount)
-////            print(self.podiumCount)
-////            print(self.poleCount)
-////            print(self.bestFinish)
-////            print(self.numOfRetire)
-////            print(self.bestGrid)
-////            print(self.nationality)
-////            print(self.fastestLapCount)
-//
-////            self.testDict(res: self.arrays)
-////            print(self.items)
-//
-//        }
     }
     
     var numOfRace = 0
@@ -174,11 +150,33 @@ final class HistoricalDriverStandingsViewController: BaseHistoricalDetailViewCon
     var driverRace = 0
     var teammateRace = 0
     
-    var a = 0
     
     func testDict(res: [[ResultF1]]) {
+        var supprots: [String] = []
+        
         for (_, array) in res.enumerated() {
             for i in array {
+                if i.driver.driverID != driver {
+                    supprots.append(i.driver.driverID)
+                }
+            }
+        }
+        
+        let teammates = supprots.dropDuplicates()
+        
+        for (_, teammate) in teammates.enumerated() {
+            if items.isEmpty {
+                items["qualification"] = [[driver : driverQuali, teammate: teammateQuali]]
+                items["race"] = [[driver : driverRace, teammate: teammateRace]]
+            } else {
+                items["qualification"]?.append([driver : driverQuali, teammate: teammateQuali])
+                items["race"]?.append([driver : driverRace, teammate: teammateRace])
+            }
+        }
+            
+        for (_, array) in res.enumerated() {
+            for i in array {
+         
                 if i.driver.driverID == driver {
                     driver = i.driver.driverID
                     driverQuali = Int(i.grid) ?? 0
@@ -210,34 +208,13 @@ final class HistoricalDriverStandingsViewController: BaseHistoricalDetailViewCon
                 teammateRace += 1
             }
             
-            if items.isEmpty {
-                items["qualification"] = [[driver : driverQuali, teammate: teammateQuali]]
-                items["race"] = [[driver : driverRace, teammate: teammateRace]]
-                //        print("If is empty: \(items)")
-            } else {
-                if var compare = items["qualification"].map ({ $0.map ({ $0.keys.contains(teammate) }) })  {
+            for (i,e) in items["qualification"]!.enumerated()  {
+                if e.keys.contains(teammate) {
+                    items["qualification"]?[i][driver]! += driverQuali
+                    items["qualification"]?[i][teammate]! += teammateQuali
                     
-//                    a += 1
-//                    print(a)
-//                    print(compare)
-//                    if compare.count > 1 {
-//                        compare.remove(at: 1)
-//                        print(compare)
-//                    }
-                    
-//                    for (_, bool) in compare.enumerated() {
-//                        if bool {
-//                            items["qualification"]![0][driver]! += driverQuali
-//                            items["qualification"]![0][teammate]! += teammateQuali
-////                            print(items["qualification"])
-//
-//                            items["race"]![0][driver]! += driverRace
-//                            items["race"]![0][teammate]! += teammateRace
-//                        } else {
-//                            items["qualification"]?.append([driver : driverQuali, teammate: teammateQuali])
-//                            items["race"]?.append([driver : driverRace, teammate: teammateRace])
-//                        }
-//                    }
+                    items["race"]?[i][driver]! += driverRace
+                    items["race"]?[i][teammate]! += teammateRace
                 }
             }
         }
