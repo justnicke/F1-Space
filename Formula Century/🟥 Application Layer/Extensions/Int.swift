6f46d1321813440244703e95de7ef5b449f07f03
Nikita <justnicke@gmail.com>
2021-03-25 18:14:07 +0300

    added extension Int

---
+++ Formula Century/🟥 Application Layer/Extensions/Int.swift
@@ -0,0 +1,16 @@
+//
+//  Int.swift
+//  Formula Century
+//
+//  Created by Nikita Sukachev on 25.03.2021.
+//  Copyright © 2021 Nikita Sukachev. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    /// Convert Int to String
+    func toString() -> String {
+        return String(self)
+    }
+}
